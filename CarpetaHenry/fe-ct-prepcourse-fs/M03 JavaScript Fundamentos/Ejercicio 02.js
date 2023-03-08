b8bec61060cf02bbe037cb823a868807aa66f924
RFabricioIsmaelBedeira <126897031+FIsmaelBedeira@users.noreply.github.com>
2023-03-08 16:41:57 -0300

    Add files via upload

--- CarpetaHenry/fe-ct-prepcourse-fs/M03 JavaScript Fundamentos/Ejercicio 02.js
+++ CarpetaHenry/fe-ct-prepcourse-fs/M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,6 +4,9 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return funtion(); {
+      return string;
+   }
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,26 +14,39 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+      return x + y;
+      console.log(x + y)
+   
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   return x - y;
+    console.log(x - y)
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   return x / y;
+   console.log(x / y)
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   return x * y;
+   console.log(x*y)
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   let x = 10;
+   let y = 3;
+   let resto = x % y
+   console.log(resto)
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
